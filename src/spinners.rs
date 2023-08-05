7ae7549747a6e77c235b9f7e613b7a31d0705a9f
Abdur-RahmaanJ <cryptolabour@gmail.com>
2023-08-05 15:50:26 +0400

    exp: rust eating life with threads

--- src/spinners.rs
+++ src/spinners.rs
@@ -1,46 +1,101 @@
-use std::collections::HashMap;
 
-pub fn frame_data() -> HashMap<&'static str, Vec<&'static str>> {
-    let frames = HashMap::from([
-    	("simple", vec!["-", "/", "|", "\\"]),
-    	("moon", vec!["🌕", "🌖", "🌘", "🌒", "🌓", "🌔"]),
-    	]);
-
-    frames
-}
-
+use std::thread;
+use std::time::Duration;
+use std::io::{self, Write};
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
 
 struct Spinner {
-    frames: Vec<&'static str>,
-    current_frame: usize,
+    frames: Vec<char>,
+    interval: Duration,
 }
 
 impl Spinner {
     fn new() -> Self {
-        Self {
+        Spinner {
             frames: vec!["🌕", "🌖", "🌘", "🌒", "🌓", "🌔"],
-            current_frame: 0,
+            interval: Duration::from_millis(100),
         }
     }
 
-    fn spin(&mut self) {
-        self.current_frame = (self.current_frame + 1) % self.frames.len();
-    }
+    fn start(&self, status: Arc<Mutex<Status>>, stop_rx: mpsc::Receiver<()>) {
+        let num_frames = self.frames.len();
+        let mut current_frame = 0;
 
-    fn update(&mut self) {
-        print!("\r{}", self.frames[self.current_frame]);
-        io::stdout().flush().unwrap();
-    }
-
-    fn start(&mut self) {
         loop {
-            self.update();
-            self.spin();
-            thread::sleep(Duration::from_millis(100));
-        }
-    }
+            let status = status.lock().unwrap();
+            print!("\r{} {}", self.frames[current_frame], status.message);
+            io::stdout().flush().unwrap();
 
-    fn stop(&self) {
-        println!();
+            current_frame = (current_frame + 1) % num_frames;
+            thread::sleep(self.interval);
+
+            if stop_rx.try_recv().is_ok() {
+                break;
+            }
+        }
     }
 }
+
+struct Status {
+    message: String,
+}
+
+impl Status {
+    fn new() -> Self {
+        Status {
+            message: String::new(),
+        }
+    }
+
+    fn log(&mut self, message: &str) {
+        self.message = message.to_string();
+    }
+}
+
+struct Live {
+    spinner: Arc<Mutex<Spinner>>,
+    status: Arc<Mutex<Status>>,
+}
+
+impl Live {
+    fn new() -> Self {
+        let spinner = Arc::new(Mutex::new(Spinner::new()));
+        let status = Arc::new(Mutex::new(Status::new()));
+
+        Live {
+            spinner,
+            status,
+        }
+    }
+
+    fn start(&self) {
+        let spinner = Arc::clone(&self.spinner);
+        let status = Arc::clone(&self.status);
+
+        let (tx, rx) = mpsc::channel();
+
+
+        let spinner_thread = thread::spawn(move || {
+            let spinner = spinner.lock().unwrap();
+            spinner.start(status.clone(), rx);
+        });
+
+        let status_thread = thread::spawn(move || {
+            for i in 1..=5 {
+                thread::sleep(Duration::from_secs(1));
+                let message = format!("Processing... Step {}", i);
+                let mut status = status.lock().unwrap();
+                status.log(&message);
+            }
+            tx.send(()).unwrap();
+        });
+
+        // Wait for the main task thread to finish
+        status_thread.join().unwrap();
+
+        // Wait for the spinner thread to finish
+        spinner_thread.join().unwrap();
+    }
+}
+
